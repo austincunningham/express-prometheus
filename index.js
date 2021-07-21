8238c999d36cbcceba28dd550a9ea454e17a787f
Austin Cunningham <aucunnin@redhat.com>
2021-07-21 09:43:23 +0100

    add promClient collectDefaultMetrics & added custom labels including time

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const app = express();
 const promBundle = require("express-prom-bundle");
 
-const metricsMiddleware = promBundle({includeMethod: true, includePath: true, includeStatusCode: true, includeUp: true});
+const metricsMiddleware = promBundle({
+    includeMethod: true, 
+    includePath: true, 
+    includeStatusCode: true, 
+    includeUp: true,
+    customLabels: {project_name: 'hello_world', project_type: 'test_metrics_labels', time: null},
+    transformLabels: labels => Object.assign(labels, {time: new Date().getTime()}),
+    promClient: {
+        collectDefaultMetrics: {
+        }
+      }
+});
 
 app.use(metricsMiddleware)
 
@@ -10,5 +21,5 @@ app.use(metricsMiddleware)
 app.get("/hello", (req,res) => res.json({hello:"world"}))
 
 app.listen(8080, function () {    
-    console.log('Listening at http://localhost:8080');  
+    console.log('Listening at http://localhost:8080/hello \nMetrics at http://localhost:8080/metrics');  
   });
